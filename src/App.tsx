3243a7377be40560fd38689ea99f0a06ee94f2ee
DALO <dalodev@proton.me>
2024-05-08 09:32:33 -0500

    feat(dark): add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { ThemeProvider } from "./components/ui/theme-provider";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <Button>Click me</Button>
-    </>
+    </ThemeProvider>
   );
 }
 
